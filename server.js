69d1fc67467f78f342e6c694a5962f8744af7bb7
Brian Song <bsong070@gmail.com>
2021-01-05 15:57:52 -0500

    test

--- server.js
+++ server.js
@@ -18,3 +18,14 @@ const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
+
+let multer = require('multer')
+
+app.post('/api/fileanalyse', multer().single('upfile'), (request, response)=>{  //multer() no input means don't save and name variable is upfile
+  console.log(request.file)  // input is type file
+  let responseObject = {}
+  responseObject['name'] = request.file.originalname
+  responseObject['type'] = request.file.mimetype
+  responseObject['size'] = request.file.size
+response.json(responseObject)
+})
